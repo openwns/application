b0ccc6658d2ba9645a3f2fcca8565d15f54b3cb7
Marc Schinnenburg <marc@schinnenburg.com>
2007-12-03 13:56:38 +0100

    modified common.py to meet new openWNS branch

--- config/common.py
+++ config/common.py
@@ -1,25 +1,14 @@
-import os
 import CNBuildSupport
-from CNBuildSupport import CNBSEnvironment
-import wnsbase.RCS as RCS
+import wnsbase
 
-commonEnv = CNBSEnvironment(PROJNAME       = 'openWNS',
-                            AUTODEPS       = ['wns'],
-                            SHORTCUTS      = True,
-                            DEFAULTVERSION = True,
-                            BINARY         = True,
-                            FLATINCLUDES   = False,
-                            LIBS           = ['wns-3.0',
-                                              'cppunit',
-                                              'pthread',
-                                              'python2.4',
-                                              'dl',
-                                              'boost_program_options',
-                                              'boost_signals',
-                                              'gsl',
-                                              'gslcblas',
-                                              ],
-                            REVISIONCONTROL = RCS.Bazaar('..', 'openWNS-core', 'main', '1.0'),
-                            )
+commonEnv = CNBuildSupport.CNBSEnvironment(PROJNAME       = 'openWNS',
+                                           AUTODEPS       = ['wns'],
+                                           SHORTCUTS      = True,
+                                           DEFAULTVERSION = True,
+                                           BINARY         = True,
+                                           FLATINCLUDES   = False,
+                                           LIBS           = ['wns-1.0'],
+                                           REVISIONCONTROL = wnsbase.rcs.RCS.Bazaar('..', 'openWNS-core', 'main', '1.0'),
+                                           )
 
 Return('commonEnv')
